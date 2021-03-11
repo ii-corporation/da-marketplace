a4da014e818510913616d8aa6a5c3b44087775fb
Alex Matson <alex.matson@digitalasset.com>
2021-03-11 16:42:53 -0500

    pull in original login page and credential management fns

--- ui2/src/index.tsx
+++ ui2/src/index.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "@material-ui/styles";
-import { CssBaseline } from "@material-ui/core";
 
 import Themes from "./themes";
 import Main from "./Main";
 import { LayoutProvider } from "./context/LayoutContext";
 import { UserProvider } from "./context/UserContext";
 
+import 'semantic-ui-css/semantic.min.css'
+import './index.scss';
+
 ReactDOM.render(
   <LayoutProvider>
     <UserProvider>
       <ThemeProvider theme={Themes.default}>
-        <CssBaseline />
         <Main defaultPath="/apps"/>
       </ThemeProvider>
     </UserProvider>
